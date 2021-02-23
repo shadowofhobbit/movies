b0922ca19c1bf9b08a90552c774f671880eef366
Iuliia <iuliiaponomareva@gmail.com>
2021-02-23 19:55:02 +0300

    Add Swagger

--- src/main/java/iuliia/movies/domain/actors/ActorController.java
+++ src/main/java/iuliia/movies/domain/actors/ActorController.java
@@ -1,5 +1,7 @@
 package iuliia.movies.domain.actors;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,32 +12,38 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/actors")
 @RequiredArgsConstructor
+@Api(tags = "Actors")
 public class ActorController {
     private final ActorService actorService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Add actor")
     public Actor create(@RequestBody @Valid Actor actor) {
         return actorService.create(actor);
     }
 
     @GetMapping
+    @ApiOperation("Get all actors")
     public Iterable<Actor> get() {
         return actorService.getAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Get actor by id")
     public Actor get(@PathVariable Long id) {
         return actorService.getById(id);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation("Update actor")
     public Actor update(@PathVariable Long id, @RequestBody Actor actor) {
         return actorService.update(id, actor);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation("Delete actor by id")
     public void delete(@PathVariable Long id) {
         actorService.deleteById(id);
     }
--- src/main/java/iuliia/movies/domain/movies/MovieController.java
+++ src/main/java/iuliia/movies/domain/movies/MovieController.java
@@ -1,5 +1,7 @@
 package iuliia.movies.domain.movies;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,32 +12,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/movies")
 @RequiredArgsConstructor
+@Api(tags="Movies")
 public class MovieController {
     private final MovieService movieService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Add movie")
     public Movie create(@RequestBody @Valid Movie movie) {
         return movieService.create(movie);
     }
 
     @GetMapping
+    @ApiOperation("Get all movies")
     public List<Movie> get() {
         return movieService.getAll();
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Get movie by id")
     public Movie get(@PathVariable Long id) {
         return movieService.getById(id);
     }
 
     @PutMapping("/{id}")
+    @ApiOperation("Update movie")
     public Movie update(@PathVariable Long id, @RequestBody @Valid Movie movie) {
         return movieService.update(id, movie);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation("Delete movie by id")
     public void delete(@PathVariable Long id) {
         movieService.deleteById(id);
     }
