2effd3e80b028c18a21185fc6f69042be963f441
Iuliia <iuliiaponomareva@gmail.com>
2021-02-23 19:55:02 +0300

    Add Swagger

--- src/main/java/iuliia/movies/domain/cast/CastController.java
+++ src/main/java/iuliia/movies/domain/cast/CastController.java
@@ -1,5 +1,6 @@
 package iuliia.movies.domain.cast;
 
+import io.swagger.annotations.ApiOperation;
 import iuliia.movies.domain.actors.Actor;
 import iuliia.movies.domain.movies.Movie;
 import lombok.RequiredArgsConstructor;
@@ -16,16 +17,19 @@ public class CastController {
     private final CastService castService;
 
     @PutMapping("/movies/{movieId}/cast/{actorId}")
+    @ApiOperation(tags = "Movies", value = "Add actor to movie cast")
     void addCastMember(@PathVariable Long actorId, @PathVariable Long movieId) {
         castService.addCastMember(actorId, movieId);
     }
 
     @GetMapping("/movies/{movieId}/cast")
+    @ApiOperation(tags = "Movies", value = "Get movie cast")
     List<Actor> getCast(@PathVariable Long movieId) {
         return castService.getCast(movieId);
     }
 
     @GetMapping("/actors/{actorId}/movies")
+    @ApiOperation(tags = "Actors", value = "Get movies with actor")
     List<Movie> getMoviesForActor(@PathVariable Long actorId) {
         return castService.getMoviesForActor(actorId);
     }
